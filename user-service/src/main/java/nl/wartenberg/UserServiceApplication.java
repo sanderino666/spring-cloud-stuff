78429f749f236ce61eb5b07ece357a3f06350041
Sander Wartenberg <sanderwartenberg@live.nl>
2016-11-14 22:44:02 +0100

    all services register to Eureka, config-service uses Git repo and user-service uses config-service to retrieve configuration

---
+++ user-service/src/main/java/nl/wartenberg/UserServiceApplication.java
@@ -0,0 +1,26 @@
+package nl.wartenberg;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@Autowired
+	private Environment environment;
+
+	@Bean
+	public String getEnvironment() {
+		System.err.println(environment.getProperty("foo"));
+		return null;
+	}
+}
