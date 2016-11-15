7f8aad4f7cab5467c5c731914063c062e4074c59
s.wartenberg@everest.nl <s.wartenberg@everest.nl>
2016-11-15 16:30:27 +0100

    trying to get oauth2 server working

--- user-service/src/main/java/nl/wartenberg/UserServiceApplication.java
+++ user-service/src/main/java/nl/wartenberg/UserServiceApplication.java
@@ -1,12 +1,9 @@
 package nl.wartenberg;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -19,14 +16,5 @@ public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
-	}
-
-	@Autowired
-	private Environment environment;
-
-	@Bean
-	public String getEnvironment() {
-		System.err.println(environment.getProperty("foo"));
-		return null;
 	}
 }
